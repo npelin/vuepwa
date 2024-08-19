b2a0dd74d026503d6dfec1e7b0091fce3f3e2cc9
npelin <innewlife@gmail.com>
2024-08-19 21:22:14 +0800

    .

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // serve:{
-  //   host: '0.0.0.0',  
-  //   port: 5174
-  // },
-})
